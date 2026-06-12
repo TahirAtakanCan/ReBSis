f700a58d7eef3c65d7f8264705bfd594cc63a61f
Tahir Atakan Can <tahiratakancan@gmail.com>
2026-06-12 10:20:01 +0300

    chore: Tauri updater + GitHub Actions release workflow

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,9 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    rebsis_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
